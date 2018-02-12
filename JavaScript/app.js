e009accf983ed2c6f34ce2345acd649d01d6fc09
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-02-12 14:49:24 -0800

    added constructor function and created new objects, need to integrate into a function

---
+++ JavaScript/app.js
@@ -0,0 +1,34 @@
+function Product(name, fileLocation, numClicks, numViews) {
+    this.name = name;
+    this.fileLocation = fileLocation;
+    this.numClicks = numClicks;
+    this.numViews = numViews;
+}
+const game = { 
+    productsArray: [],
+    
+const bag = new Product('R2-D2 Luggage', /img/bag.jpg, 0, 0);
+const banana = new Product('Banana Slicer', /img/banana.jpg, 0, 0);
+const bathroom = new Product('Bathroom iPad Holder', /img/bathroom.jpg, 0, 0);
+const boots = new Product('Waterproof Converse Boots', /img/boots.jpg, 0, 0);
+const breakfast = new Product('Instant Breakfast', /img/breakfast.jpg, 0, 0);
+const bubblegum = new Product('Meatball Bubblegum', /img/bubblegum.jpg, 0, 0);
+const chair = new Product('Retro Chair', /img/chair.jpg, 0, 0);
+const cthuluhu = new Product('Cthuluhu Figurine', /img/cthuluh.jpg, 0, 0);
+const dogDuck = new Product('Duckbill for Dogs', /img/dog-duck.jpg, 0, 0);
+const dragonMeat = new Product('Dragon Meat', /img/dragon.jpg, 0, 0);
+const pen = new Product('Utinsel Adapter for Pens', /img/pens.jpg, 0, 0);
+const petSweep = new Product('Pet Sweep', /img/pet-sweep.jpg, 0, 0);
+const scissors = new Product('Pizza Scissors', /img/scissors.jpg, 0, 0);
+const shark = new Product('Shark Sleeping Bag', /img/shark.jpg, 0, 0);
+const sweep = new Product('Baby Sweeper', /img/sweep.png, 0, 0);
+const tantaun = new Product('Tantaun Sleeping Bag', /img/tantaun.jpg, 0, 0);
+const unicorn = new Product('Unicorn Meat', /img/unicorn.jpg, 0, 0);
+const usb = new Product('Tentacle USB Drive', /img/usb.gif, 0, 0);
+const waterCan = new Product('Infinite Water Can', /img/water-can.jpg, 0, 0);
+const wineGlass = new Product('Death Star Wine Glass', /img/wine-glass.jpg, 0, 0);
+
+
+
+
+}
