fdf90cd1b89b9d495a44f1ea876771f6f1950ec3
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-02-12 15:16:12 -0800

    pushed products into array successfully

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,34 +1,37 @@
-function Product(name, fileLocation, numClicks, numViews) {
+function Product(name, fileLocation) {
     this.name = name;
     this.fileLocation = fileLocation;
-    this.numClicks = numClicks;
-    this.numViews = numViews;
+    this.numClicks = 0;
+    this.numViews = 0;
 }
-const game = { 
+const tracking = {
     productsArray: [],
-    
-const bag = new Product('R2-D2 Luggage', /img/bag.jpg, 0, 0);
-const banana = new Product('Banana Slicer', /img/banana.jpg, 0, 0);
-const bathroom = new Product('Bathroom iPad Holder', /img/bathroom.jpg, 0, 0);
-const boots = new Product('Waterproof Converse Boots', /img/boots.jpg, 0, 0);
-const breakfast = new Product('Instant Breakfast', /img/breakfast.jpg, 0, 0);
-const bubblegum = new Product('Meatball Bubblegum', /img/bubblegum.jpg, 0, 0);
-const chair = new Product('Retro Chair', /img/chair.jpg, 0, 0);
-const cthuluhu = new Product('Cthuluhu Figurine', /img/cthuluh.jpg, 0, 0);
-const dogDuck = new Product('Duckbill for Dogs', /img/dog-duck.jpg, 0, 0);
-const dragonMeat = new Product('Dragon Meat', /img/dragon.jpg, 0, 0);
-const pen = new Product('Utinsel Adapter for Pens', /img/pens.jpg, 0, 0);
-const petSweep = new Product('Pet Sweep', /img/pet-sweep.jpg, 0, 0);
-const scissors = new Product('Pizza Scissors', /img/scissors.jpg, 0, 0);
-const shark = new Product('Shark Sleeping Bag', /img/shark.jpg, 0, 0);
-const sweep = new Product('Baby Sweeper', /img/sweep.png, 0, 0);
-const tantaun = new Product('Tantaun Sleeping Bag', /img/tantaun.jpg, 0, 0);
-const unicorn = new Product('Unicorn Meat', /img/unicorn.jpg, 0, 0);
-const usb = new Product('Tentacle USB Drive', /img/usb.gif, 0, 0);
-const waterCan = new Product('Infinite Water Can', /img/water-can.jpg, 0, 0);
-const wineGlass = new Product('Death Star Wine Glass', /img/wine-glass.jpg, 0, 0);
+    start: function() {
+        this.productsArray.push(
+            new Product('R2-D2 Luggage', '/img/bag.jpg'),
+            new Product('Banana Slicer', '/img/banana.jpg'),
+            new Product('Bathroom iPad Holder', '/img/bathroom.jpg'),
+            new Product('Waterproof Converse Boots', '/img/boots.jpg'),
+            new Product('Instant Breakfast', '/img/breakfast.jpg'),
+            new Product('Meatball Bubblegum', '/img/bubblegum.jpg'),
+            new Product('Retro Chair', '/img/chair.jpg'),
+            new Product('Cthuluhu Figurine', '/img/cthuluh.jpg'),
+            new Product('Duckbill for Dogs', '/img/dog-duck.jpg'),
+            new Product('Dragon Meat', '/img/dragon.jpg'),
+            new Product('Utinsel Adapter for Pens', '/img/pens.jpg'),
+            new Product('Pet Sweep', '/img/pet-sweep.jpg'),
+            new Product('Pizza Scissors', '/img/scissors.jpg'),
+            new Product('Shark Sleeping Bag', '/img/shark.jpg'),
+            new Product('Baby Sweeper', '/img/sweep.png'),
+            new Product('Tantaun Sleeping Bag', '/img/tantaun.jpg'),
+            new Product('Unicorn Meat', '/img/unicorn.jpg'),
+            new Product('Tentacle USB Drive', '/img/usb.gif'),
+            new Product('Infinite Water Can', '/img/water-can.jpg'),
+            new Product('Death Star Wine Glass', '/img/wine-glass.jpg')
+        );
+        console.table(this.productsArray);
+    }
 
+};
 
-
-
-}
+tracking.start();
