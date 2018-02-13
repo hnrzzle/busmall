dd6d6ff1e95cf5fd74e07e7eef3afbc5450427cb
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-02-12 17:20:13 -0800

    fixed bug, script was running before html

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,6 +1,6 @@
-function Product(name, fileLocation) {
+function Product(name, imgURL) {
     this.name = name;
-    this.fileLocation = fileLocation;
+    this.imgURL = imgURL;
     this.numClicks = 0;
     this.numViews = 0;
 }
@@ -8,41 +8,51 @@ const tracking = {
     productsArray: [],
     start: function() {
         this.productsArray.push(
-            new Product('R2-D2 Luggage', '/img/bag.jpg'),
-            new Product('Banana Slicer', '/img/banana.jpg'),
-            new Product('Bathroom iPad Holder', '/img/bathroom.jpg'),
-            new Product('Waterproof Converse Boots', '/img/boots.jpg'),
-            new Product('Instant Breakfast', '/img/breakfast.jpg'),
-            new Product('Meatball Bubblegum', '/img/bubblegum.jpg'),
-            new Product('Retro Chair', '/img/chair.jpg'),
-            new Product('Cthuluhu Figurine', '/img/cthuluh.jpg'),
-            new Product('Duckbill for Dogs', '/img/dog-duck.jpg'),
-            new Product('Dragon Meat', '/img/dragon.jpg'),
-            new Product('Utinsel Adapter for Pens', '/img/pens.jpg'),
-            new Product('Pet Sweep', '/img/pet-sweep.jpg'),
-            new Product('Pizza Scissors', '/img/scissors.jpg'),
-            new Product('Shark Sleeping Bag', '/img/shark.jpg'),
-            new Product('Baby Sweeper', '/img/sweep.png'),
-            new Product('Tantaun Sleeping Bag', '/img/tantaun.jpg'),
-            new Product('Unicorn Meat', '/img/unicorn.jpg'),
-            new Product('Tentacle USB Drive', '/img/usb.gif'),
-            new Product('Infinite Water Can', '/img/water-can.jpg'),
-            new Product('Death Star Wine Glass', '/img/wine-glass.jpg')
+            new Product('R2-D2 Luggage', 'img/bag.jpg'),
+            new Product('Banana Slicer', 'img/banana.jpg'),
+            new Product('Bathroom iPad Holder', 'img/bathroom.jpg'),
+            new Product('Waterproof Converse Boots', 'img/boots.jpg'),
+            new Product('Instant Breakfast', 'img/breakfast.jpg'),
+            new Product('Meatball Bubblegum', 'img/bubblegum.jpg'),
+            new Product('Retro Chair', 'img/chair.jpg'),
+            new Product('Cthuluhu Figurine', 'img/cthulhu.jpg'),
+            new Product('Duckbill for Dogs', 'img/dog-duck.jpg'),
+            new Product('Dragon Meat', 'img/dragon.jpg'),
+            new Product('Utinsel Adapter for Pens', 'img/pen.jpg'),
+            new Product('Pet Sweep', 'img/pet-sweep.jpg'),
+            new Product('Pizza Scissors', 'img/scissors.jpg'),
+            new Product('Shark Sleeping Bag', 'img/shark.jpg'),
+            new Product('Baby Sweeper', 'img/sweep.png'),
+            new Product('Tantaun Sleeping Bag', 'img/tauntaun.jpg'),
+            new Product('Unicorn Meat', 'img/unicorn.jpg'),
+            new Product('Tentacle USB Drive', 'img/usb.gif'),
+            new Product('Infinite Water Can', 'img/water-can.jpg'),
+            new Product('Death Star Wine Glass', 'img/wine-glass.jpg')
         );
         this.showProduct();
     },
     showProduct: function () {
-
+        const section = document.getElementById('game-board');
         const selectedProducts = [];
         while (selectedProducts.length < 3) {
             const randomNum = Math.floor(Math.random() * (this.productsArray.length));
-            const displayProduct = this.productsArray[randomNum];
-            if (selectedProducts.includes(displayProduct)) continue;
-            selectedProducts.push(displayProduct);
+            const randomProduct = this.productsArray[randomNum];
+            if (selectedProducts.includes(randomProduct)) continue;
+            selectedProducts.push(randomProduct);
+            console.log(randomProduct.createTag());
+            section.appendChild(randomProduct.createTag());
         };
         console.table(selectedProducts);
     }
 
 };
+
+Product.prototype.createTag = function () {
+    const element = document.createElement('img');
+    element.src = this.imgURL;
+    element.setAttribute('alt', this.name);
+    return element;
+};
+
 
 tracking.start();
