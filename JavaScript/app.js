6576f05c15924cc1064fc0e99c0c7c8c6124ae87
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-02-13 18:50:56 -0800

    changed chart to be vertical

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -49,7 +49,6 @@ const tracking = {
                 tracking.clearBoard();
                 tracking.showProduct();
             } else {
-                alert('You\'re out of clicks!');
                 tracking.clearBoard();
                 tracking.finishText();
                 board.removeEventListener('click', event.target);
@@ -88,27 +87,14 @@ const tracking = {
         const chartCanvas = document.getElementById('chart');
         const chartCtx = chartCanvas.getContext('2d');
         const chart = new Chart (chartCtx, {
-            type: 'bar',
+            type: 'horizontalBar',
             data: {
                 labels: this.productNames(),
                 datasets: [{
                     label: '# of Votes',
                     data: this.productClicks(),
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
+                    backgroundColor: ('white'),
                     borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
                     ],
                     borderWidth: 1
                 }]
