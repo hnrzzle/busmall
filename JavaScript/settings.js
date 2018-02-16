d37ea7370c956d288b7a0ae589dafe8a84e3e042
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-02-16 08:50:46 -0800

    added conditional forcing user to input both forms, else would break code

--- JavaScript/settings.js
+++ JavaScript/settings.js
@@ -9,22 +9,24 @@ settingsForm.addEventListener('submit', function() {
     const productAmt = parseInt(this['product-amount'].value);
     const roundsAmt = parseInt(this['rounds-amount'].value);
 
-    const settings = {
-        prodAmt: productAmt,
-        roundAmt: roundsAmt,
-    };
-    localStorage.setItem('settings', JSON.stringify(settings));
-    console.log(settings);
 
-    if (this['product-amount'].value || this['rounds-amount'].value) {
+    if (this['product-amount'].value && this['rounds-amount'].value) {
 
         p.textContent = 'Your changes were successfully saved!';
         error.appendChild(p);
 
+        const settings = {
+            prodAmt: productAmt,
+            roundAmt: roundsAmt,
+        };
+        localStorage.setItem('settings', JSON.stringify(settings));
+        console.log(settings);
+
         console.log('yes');
 
+
     } else {
-        p.textContent = 'Please input a value!';
+        p.textContent = 'Please input a value for both settings!';
         error.appendChild(p);
         console.log('no');
     }
