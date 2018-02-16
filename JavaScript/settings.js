16ba55d4e7180d2ced47b089a71c59e6f6cd44b3
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-02-16 08:39:08 -0800

    accidentally deleted local storage line lol, error success message works

--- JavaScript/settings.js
+++ JavaScript/settings.js
@@ -1,7 +1,11 @@
 const settingsForm = document.getElementById('submission');
 settingsForm.addEventListener('submit', function() {
-
     event.preventDefault();
+    
+    const error = document.getElementById('error-success');
+        
+    const p = document.createElement('p');
+    error.innerHTML = '';
 
     const productAmt = parseInt(this['product-amount'].value);
     const roundsAmt = parseInt(this['rounds-amount'].value);
@@ -10,17 +14,20 @@ settingsForm.addEventListener('submit', function() {
         prodAmt: productAmt,
         roundAmt: roundsAmt,
     };
-    if (this['product-amount'].value) {
-        const error = document.getElementById('error-success');
-        const p = document.createElement('p');
+    localStorage.setItem('settings', JSON.stringify(settings));
+    console.log(settings);
+
+    if (this['product-amount'].value || this['rounds-amount'].value) {
+        
         p.textContent = 'Your changes were successfully saved!';
         error.appendChild(p);
 
         console.log('yes');
 
     } else {
+        p.textContent = 'Please input a value!';
+        error.appendChild(p);
         console.log('no');
     }
-
 });
 
